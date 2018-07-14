03dc526e48277eb9fabed078648a3cd7c2a44cb5
frontjss <frontjss@gmail.com>
2018-07-14 19:34:12 +0300

    new

--- filter/urls.py
+++ filter/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.urls import path
+from django.contrib import admin
 from django.conf.urls.static import static
 
 urlpatterns = [
