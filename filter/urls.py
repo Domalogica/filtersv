514d16229611f6ca35db14a67dcccb3482205aae
frontjss <frontjss@gmail.com>
2018-07-14 19:20:44 +0300

    new

--- filter/urls.py
+++ filter/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('filter.shop.urls')),
+    path('', include('filter.shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
