081c1afc540e3d203327b16a6b030e8cabb74da5
frontjss <frontjss@gmail.com>
2018-07-14 19:15:38 +0300

    new

--- filter/urls.py
+++ filter/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'filter'
+app_name = 'filter.shop'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
