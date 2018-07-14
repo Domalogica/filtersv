5b762eb3c303a0c1a21cbfd40866c920be7636f0
frontjss <frontjss@gmail.com>
2018-07-14 19:55:06 +0300

    new

--- filter/urls.py
+++ filter/urls.py
@@ -17,11 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from . import shop
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('shop.urls', namespace='shop'))
+    url(r'^', include('filter.shop.urls', namespace='shop'))
 ]
 
 if settings.DEBUG:
