605d9fb2d13b654357d02449ea172b89771f10de
frontjss <frontjss@gmail.com>
2018-07-14 21:54:58 +0300

    new

--- filter/settings.py
+++ filter/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop'
+    'filter.shop'
 ]
 
 MIDDLEWARE = [
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'filter.wsgi.application'
 
 
 # Database
