e7119d58de63f8bdf9bdea22f4bc1328bd538e87
frontjss <frontjss@gmail.com>
2018-07-15 17:02:32 +0300

    new

--- filter/cart/views.py
+++ filter/cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from filter.shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from django.views.decorators.csrf import csrf_exempt
 
 
 @require_POST
