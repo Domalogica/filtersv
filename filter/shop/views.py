efe967c94cc63fb4bc0da0acf8092fe38b9e6dd2
frontjss <frontjss@gmail.com>
2018-07-15 16:54:51 +0300

    new

--- filter/shop/views.py
+++ filter/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from filter.cart.forms import CartAddProductForm
 
 # Страница с товарами
 def ProductList(request, category_slug=None):
