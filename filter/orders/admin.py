b20cd393e9d5ef8d512d607489c50a6f72a7f214
frontjss <frontjss@gmail.com>
2018-07-16 16:13:50 +0300

    new

--- filter/orders/admin.py
+++ filter/orders/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.http import HttpResponse
+import csv
+import datetime
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
+
+
+def ExportToCSV(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; \
+        filename=Orders-{}.csv'.format(datetime.datetime.now().strftime("%d/%m/%Y"))
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Первая строка- оглавления
+    writer.writerow([field.verbose_name for field in fields])
+    # Заполняем информацией
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+    ExportToCSV.short_description = 'Export CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -7,9 +35,16 @@ class OrderItemInline(admin.TabularInline):
     raw_id_field = ['product']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = [..., OrderDetail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [ExportToCSV]
 
 admin.site.register(Order, OrderAdmin)
+
+
+
+def OrderDetail(obj):
+    return format_html('<a href="{}">Посмотреть</a>'.format(
+        reverse('orders:AdminOrderDetail', args=[obj.id])
+    ))
--- filter/orders/views.py
+++ filter/orders/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from .models import OrderItem
+from django.shortcuts import render, get_object_or_404
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import OrderCreated
 from filter.cart.cart import Cart
+from django.contrib.admin.views.decorators import staff_member_required
 
+
+@staff_member_required
+def AdminOrderDetail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
 
 def OrderCreate(request):
     cart = Cart(request)
